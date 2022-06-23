ac4c53362e450445f5e09e63cdfa54cae70c6553
Alex <sahanc@yandex.ru>
2022-06-23 15:49:06 +0500

    first commit

---
+++ conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: en or ru")
+
+@pytest.fixture
+def browser(request):
+    browser_language = request.config.getoption("language")
+    browser = None
+    if browser_language == "en":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': "en"})
+        browser = webdriver.Chrome(options=options)
+
+    elif browser_language == "ru":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': "ru"})
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError("--browser_name should be ru or en")
+
+    print("\nstart browser for test..")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
